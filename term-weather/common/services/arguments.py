d600f774ad593518297d2f72f747dedc990e4a12
Michał Szóstak <mszostakk01@gmail.com>
2020-06-28 12:55:39 +0200

    add user arguments

--- term-weather/common/services/arguments.py
+++ term-weather/common/services/arguments.py
@@ -1,6 +1,9 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--now', 'display only current weather')
-parser.add_argument('--tommorow', 'display only tommorow weather')
-parser.add_argument('--daily', 'display forecast for next 7 days')
+parser.add_argument('--now', action='store_true', help='display only current weather')
+parser.add_argument('--tommorow', action='store_true', help='display only tommorow weather')
+parser.add_argument('--daily', action='store_true', help='display forecast for next 7 days')
+
+def parse():
+  return parser.parse_args()
--- term-weather/main.py
+++ term-weather/main.py
@@ -1,7 +1,25 @@
 import common.services.config as cfg
 from common.services.weather_provider import WeatherProvider
+import common.services.arguments as ap
+
+
 
 config = cfg.load()
+args = ap.parse()
 wp = WeatherProvider(config)
-
 data = wp.fetchWeather()
+
+if args.now:
+  currentWeather = data['current'].toString()
+  print(currentWeather)
+  exit()
+
+if args.tommorow:
+  tommorowWeather = data['daily'][0].toString()
+  print(tommorowWeather)
+  exit()
+
+if args.daily:
+  for day in data['daily']:
+    print(day.toString())
+  exit()
